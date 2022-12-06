6f581c3ebdf39a83de212f926047490cf35b5792
irwanxploit7 <noware011@gmail.com>
2022-12-06 13:36:24 +0700

    sad

---
+++ plugins/main-menu2.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const fetch = require('node-fetch')
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let tag = `https://wa.me/6289653633058`
+  let mentionedJid = [m.sender]
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+let suka = `Saya Rull`
+global.fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': suka, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${suka},;;;\nFN:${suka},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': fs.readFileSync('./thumbnail.jpg'), thumbnail: fs.readFileSync('./thumbnail.jpg'),sendEphemeral: true}}}
+
+let str = `*Hai 🙂*`
+let isi =`I'm WanzBOT Yang Di Buat Gak Niat Oleh ©wazn Karena Males Recode Jadi Pake Aja Apa Yang Ada Yah .\nKalau Ada Yg Error Bisa Langsung Report Ke Owner.\n${tag} (Owner)`
+let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.send3ButtonImg(m.chat, `https://telegra.ph/file/21f148af8e3551ede03b8.jpg`, str, isi, 'Menu', '.command', 'Owner', '.owner', 'Credit', '.credit', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: 'https://youtube.com/@user-ml7dz2nm4g',
+    mediaType: 2, 
+    description: urlnya,
+    title: "Subs Yete Gw Banh:)",
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: 'https://youtube.com/@user-ml7dz2nm4g'
+     }}
+  })
+          }
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command =  /^(menu)$/i
+
+handler.register = false
+
+module.exports = handler
